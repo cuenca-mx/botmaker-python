76b176d53b39ace3c97ceb7fcb62481318a4b92e
Felipe López <flh.1989@gmail.com>
2019-03-27 20:10:52 -0600

    add assert

--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -8,6 +8,7 @@ def test_check_whatsapp_contacts(client):
     contacts = ['+55 1 55 1234 5678', '123']
     result = client.check_whatsapp_contacts('5215500000000', contacts)
     assert '+55 1 55 1234 5678' in result  # whatsapp
+    assert result['+55 1 55 1234 5678'] == '5515512345678'
     assert '123' not in result  # no whatsapp
 
 
