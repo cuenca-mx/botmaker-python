42ec0a4682e8890d76c03293199a002a5b5cd59b
Felipe López <flh.1989@gmail.com>
2019-03-27 20:06:03 -0600

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='botmaker',
-    version='0.2.2',
+    version='0.3.0',
     author='Cuenca',
     author_email='dev@cuenca.com',
     description='BotMaker API Client',
